8bf6f3e9d0b3426a3b337bc060a3523a6160d331
Chandini <139709600+A-Chandini-Devi@users.noreply.github.com>
2026-02-25 09:38:11 +0530

    Create Starte.py
    data frame

---
+++ Starte.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = pd.DataFrame(
+    {
+        "Name" : [
+            "Chandini",
+            "Devi"
+        ],
+        "Age" : [21, 20],
+        "Gender" : ["Female", "Female"]
+    }
+)
+data
+
+# ---
+# Output: 
+# Name	Age	Gender
+# 0	Chandini	21	Female
+# 1	Devi	20	Female
+# ---
